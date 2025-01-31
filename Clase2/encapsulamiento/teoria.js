0c18651bcb74e7e4542bbf7aee9746026cfd1d4a
Victor Kneider <victorkneider@gmail.com>
2025-01-31 02:40:33 -0400

    feat:add teoria.js

---
+++ Clase2/encapsulamiento/teoria.js
@@ -0,0 +1,14 @@
+/*
+1.1 ¿Qué es el encapsulamiento?
+El encapsulamiento es un principio de la programación que consiste en ocultar detalles internos de un módulo o componente, exponiendo solo lo necesario para su uso.
+✅ Ventajas del encapsulamiento:
+•	Modularidad: Permite dividir el código en bloques organizados y reutilizables.
+•	Reutilización: Reduce la duplicación de código y mejora la mantenibilidad.
+•	Mantenimiento: Facilita la detección y corrección de errores.
+✅ Ejemplo Real:
+Un televisor tiene circuitos internos que procesan la señal, pero tú solo ves la imagen y cambias los canales con el control remoto.
+
+NO METAS CON ESTO QUE NO TE IMPORTA
+SI NO TE COMPETE, NO TE METAS, POR ALGO ESTÁ OCULTO
+
+*/
